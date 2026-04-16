cd93fe9b01a223a8ac2535ca2b7c7252966be28b
KAYANE CRISTINE AGUIAR DE BRITO <kayane.brito@aluno.senac.rio>
2026-04-16 18:48:59 -0300

    Atualizando com a correção da atividade 02 aula_06

--- aula_06_01/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-resposta= 'S'
-while resposta != 'N':
-    venda=float(input('Insira o valor da venda: '))
-    if venda >= 1000:
-        desc = venda* 0.1 
-        total= venda - desc
-        print (total)
-    #soma += vend
-    resposta = input('Quer continuar?  [S/N] ').upper().strip()[0]
